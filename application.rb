4839efe895874f437fc171df2c57580694067118
Kavita Jadhav <kavita_madhukar_jadhav@external.mckinsey.com>
2021-09-21 16:39:40 +0530

    Added Application class.

--- application.rb
+++ application.rb
@@ -1,24 +1,11 @@
-require './lib/board'
-require './lib/player'
-require './lib/dice'
+require './lib/game'
 
-class Game
+class Application
   TURNS = 10
 
-  def initialize
-    @board = Board.setup
-    @player = Player.new
-  end
-
-  def start
-    for i in 1.upto(TURNS)
-      dice_output = Dice.roll
-      next_cell = @board.get_cell(@player.current_cell, dice_output)
-      @player.current_cell = next_cell
-
-      puts "Turn #{i}, dice value: #{dice_output}, moved to #{ @player.current_cell.value}"
-    end
+  def self.start
+    Game.new(TURNS).play
   end
 end
 
-Game.new.start
+Application.start
