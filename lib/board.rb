dd8cd0c162146dfb80df49b8691444cce6f35fdf
Kavita Jadhav <kavita_madhukar_jadhav@external.mckinsey.com>
2021-09-21 08:17:02 +0530

    Adds method in board calss to get next cell.

--- lib/board.rb
+++ lib/board.rb
@@ -7,6 +7,15 @@ class Board
     @cells = []
   end
 
+  def get_cell(from_cell, positions)
+    if from_cell
+      current_cell_index = from_cell.value - 1
+      @cells[current_cell_index + positions]
+    else
+      @cells[positions - 1]
+    end
+  end
+
   def self.setup
     board = Board.new
     for i in 1.upto(100)
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,13 +1,29 @@
 require 'board'
 
 RSpec.describe Board, 'model' do
-  let(:board) {Board.new}
+  let(:board) {Board.setup}
 
   describe '.setup' do
     it 'should create board and set cells in it' do
-      board = Board.setup
-
       expect(board.cells.map(&:value)).to match_array((1..100).to_a)
+    end
+  end
+
+  describe '#get_cell' do
+    context 'when first turn' do
+      it 'should return cell with given position' do
+        cell = board.get_cell(nil, 5)
+        expect(cell.value).to eq(5)
+      end
+    end
+
+    context 'when current cell present' do
+      it 'should return cell after given position' do
+        cell = Cell.new(5)
+
+        cell = board.get_cell(cell, 5)
+        expect(cell.value).to eq(10)
+      end
     end
   end
 end
